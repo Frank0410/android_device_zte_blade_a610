c8422f9e95ffd66593c02b169a052b96bae4a522
Frank0410 <33846159+Frank0410@users.noreply.github.com>
2017-12-03 10:04:37 -0600

    Update for lineageOS15 source

--- device.mk
+++ device.mk
@@ -28,16 +28,16 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # default.prop
-ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.secure=0 \
-    ro.allow.mock.location=1 \
-    ro.debuggable=1 \
-    ro.adb.secure=0 \
-    persist.service.acm.enable=0 \
-    persist.sys.usb.config=mtp \
-    ro.mount.fs=EXT4 \
-    camera.disable_zsl_mode=1 \
-    persist.sys.display.clearMotion=0 
+#ADDITIONAL_DEFAULT_PROPERTIES += \
+ #   ro.secure=0 \
+ #   ro.allow.mock.location=1 \
+ #   ro.debuggable=1 \
+ #   ro.adb.secure=0 \
+ #   persist.service.acm.enable=0 \
+ #   persist.sys.usb.config=mtp \
+ #   ro.mount.fs=EXT4 \
+ #   camera.disable_zsl_mode=1 \
+ #  persist.sys.display.clearMotion=0 
 
 # build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
